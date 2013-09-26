bdb9f8186cedf0e92f640828ce0ba599325cd76f
Branden Ogata <branden.s.ogata@gmail.com>
2013-09-25 21:57:34 -1000

    Updates the JavaScript for A12 in order to include tags that are not in the body of the loaded HTML page.

--- a12/script.js
+++ a12/script.js
@@ -7,15 +7,22 @@ $(document).ready(function()
     var value = $("#url").val();
     $.get(value, function(data)
                  {
-                   alert(data);
-                   var retrieved = $("*", $(data));
+                   // data is actually a String and not an element,
+                   // which somehow makes the * selector only select
+                   // elements in the body of the returned HTML;
+                   // this puts data into a div for jQuery to handle
+                   // in a more beneficial fashion
+                   var wrapper = document.createElement("div");
+                   $(wrapper).html(data);
+                   var pageStart = wrapper;
+                   
+                   var retrieved = $("*", pageStart);
                    var tagNames = [];
                    var tagCounts = [];
                    
                    for (var i = 0; i < retrieved.length; i++)
                    {
                      var tagName = $(retrieved).get(i).nodeName;
-                     alert(tagName);
                      
                      // If the tag does not exist in tagNames, then create a new entry
                      if (tagNames.indexOf(tagName) == -1)
@@ -54,12 +61,12 @@ $(document).ready(function()
                    
                    $("#input").append(table);
                    /*
-                   var count = $("<p></p>").text("Total tags: " + tagCount);
-                   $("#input").append(count);
-                   */
+var count = $("<p></p>").text("Total tags: " + tagCount);
+$("#input").append(count);
+*/
                  });
-    event.preventDefault();             
-  });  
+    event.preventDefault();
+  });
   
   $("#js").click(function()
   {
@@ -67,13 +74,23 @@ $(document).ready(function()
     var value = $("#url").val();
     $.get(value, function(data)
                  {
-                   var retrieved = $("script", $(data));
+                   var wrapper = document.createElement("div");
+                   $(wrapper).html(data);
+                   var pageStart = wrapper;
+                   
+                   // Note that this does not appear to handle script tags within <!-- -->,
+                   // so something like <!-- [if lt IE 9] <script>...</script> -->
+                   // is ignored and will probably cost this developer points for this assignment.
+                   var retrieved = $("script", pageStart);
                    
                    var scriptList = $("<ul></ul>").attr("id", "part2");
                    
                    for (var i = 0; i < retrieved.length; i++)
                    {
                      // If the script tag includes a src attribute, then it links to an external file
+                     // (some developer could theoretically include a src attribute and still insert
+                     //  inline JavaScript, but the assignment specified that this should list all 
+                     //  external JavaScript files, not that it should exclude all inline JavaScript)
                      if ($(retrieved[i]).attr("src") != null)
                      {
                        var externalScript = $("<li></li>").text($(retrieved[i]).attr("src"));
@@ -82,13 +99,13 @@ $(document).ready(function()
                    }
                    
                    $("#input").append(scriptList);
-                 });                   
+                 });
   });
 });
 
 /*
- * Removes the results from Parts 1 and 2 from the page.
- */
+* Removes the results from Parts 1 and 2 from the page.
+*/
  
 function removeResults()
 {
